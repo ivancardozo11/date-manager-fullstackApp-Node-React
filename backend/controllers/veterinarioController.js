56c30d25384103cff6f404d9b26e3df7a398ca6a
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-11-27 10:57:36 +0100

    token can be usen only once and when get in the DB change confirm to true and delete previous token

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -22,9 +22,24 @@ const perfil = (req, res) =>{
     res.json({ msg: "Mostrando perfil de usuarios.." });
 };
 
-const confirmar = (req, res) =>{
-    console.log(req.params.token)
-    res.json({msg: 'Confirmando cuenta...'});
+const confirmar = async (req, res) =>{
+    const { token } = req.params;
+
+    const usuarioConfirmar = await Veterinario.findOne({ token });
+    console.log(usuarioConfirmar)
+    if (!usuarioConfirmar) {
+        const error = new Error("Token no valido");
+        return res.status(404).json({ msg: error.message });
+    }
+    try {
+
+        usuarioConfirmar.token = null;
+        usuarioConfirmar.confirmado = true;
+        await usuarioConfirmar.save();
+        res.json({msg: 'Confirmando cuenta...'});
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export{
