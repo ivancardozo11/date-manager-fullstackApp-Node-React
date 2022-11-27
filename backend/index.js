9de164ffc90e761a082f41b55c92707997c684f9
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-11-27 07:11:31 +0100

    add database conection add ORM Mongoose an and dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import dotenv from 'dotenv';
+import conectarDB from "./config/db.js";
 
 const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 4000
+
+
+conectarDB();
 
 app.use('/', (req,res)=>{
     res.send("Hola mundo");
 })
-app.listen(4000, ()=>{
-    console.log("Server running and working on port 4000 :🚀🚀🚀🚀")
+
+app.listen(PORT, ()=>{
+    console.log(`Server running and working on port ${PORT} :🚀🚀🚀🚀`)
 });
