7bf02251d9df11a3d0d8b4bf606dd0122d83f38d
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-11-30 12:32:39 +0100

    checks if user exist and creates a new token that would be later send through an email

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,5 +1,6 @@
 import Veterinario from "../models/Veterinario.js";
 import generarJWT from "../helpers/generarJWT.js";
+import generarId from "../helpers/generarId.js";
 
 const registrar = async (req, res) =>{
 
@@ -69,8 +70,25 @@ const autenticar = async (req, res)=>{
     }
 };
 
-const olvidePassword = (req, res)=>{
+const olvidePassword = async (req, res)=>{
+    const { email }= req.body;
 
+    const existeVeterinario = await Veterinario.findOne({email})
+
+    if (!existeVeterinario) {
+        const error = new Error('El usuario no existe');
+        return res.status(400).json({msg: error.message});
+    }
+
+    try {
+        existeVeterinario.token = generarId();
+        await existeVeterinario.save();
+        res.json({msg: 'Hemos enviado un mail con las instrucciones'});
+        
+    } catch (error) {
+        console.log(error);
+    }
+    console.log(email);
 }
 const comprobarToken = (req, res)=>{
 
