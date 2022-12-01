821d72190f212b0b6f5c842c00afa84b02980739
Ivan Cardozo <ivancardozo11@hotmail.com>
2022-12-01 19:46:48 +0100

    update patient and validate request

--- backend/controllers/PacienteController.js
+++ backend/controllers/PacienteController.js
@@ -23,21 +23,42 @@ const obtenerPaciente = async(req, res)=>{
     const { id } = req.params;
     const paciente = await Paciente.findById(id);
 
+    if(!paciente){
+        res.status(404).json({msg: 'No encontrado'});
+    }
+
     if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
        return res.json({msg: 'Acción no válida'});
-    }
-    if (paciente) {
-        res.json(paciente);
+    
     }
 
-    console.log(paciente);
+    res.json(paciente);
 };
 const actualizarPaciente = async(req, res)=>{
-    const pacientes = await Paciente.find()
-    .where('veterinario')
-    .equals(req.veterinario);
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
 
-    res.json(pacientes);
+    if(!paciente){
+        res.status(404).json({msg: 'No encontrado'});
+    }
+
+    if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+       return res.json({msg: 'Acción no válida'});
+    
+    }
+
+    paciente.nombre = req.body.nombre || paciente.nombre;
+    paciente.propietario = req.body.propietario || paciente.propietario;
+    paciente.email = req.body.email || paciente.email;
+    paciente.fecha = req.body.fecha || paciente.fecha;
+    paciente.sintomas = req.body.sintomas || paciente.sintomas;
+
+    try {
+        const pacienteActualizado = await paciente.save();
+        res.json(pacienteActualizado);
+    } catch (error) {
+        console.log(error);
+    }
 };
 const eliminarPaciente = async(req, res)=>{
     const pacientes = await Paciente.find()
@@ -53,4 +74,4 @@ export{
     obtenerPaciente,
     actualizarPaciente,
     eliminarPaciente
-};
+}
